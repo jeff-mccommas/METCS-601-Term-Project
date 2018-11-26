2b3975ca0f4c4516afa17148b7372dd098aa3df2
jeff-mccommas <jeffmccommas@gmail.com>
2018-11-26 12:15:42 -0500

    finnish gallery toggle button

--- Scripts/gallery.js
+++ Scripts/gallery.js
@@ -1,9 +1,19 @@
-function $(img) {
-    
-}
 
-$(function(){
-    $("button").click(function(){
-        $("img").toggleClass("hidden");
+$(function() {
+    $("button.filter-btn").click((function () {
+        const grid = document.getElementById("grid");
+        const images = document.getElementsByTagName('img')
+        $("img").fadeToggle("fast");
+
+    }))
     });
-});
+
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("#div1").fadeToggle();
+//         $("#div2").fadeToggle("slow");
+//         $("#div3").fadeToggle(3000);
+//     });
+// });
+$("p").hide();
