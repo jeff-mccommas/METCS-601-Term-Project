7456a8c9e8966699045b0582340ac660e998a59c
intel9911 <tiop08317@gmail.com>
2018-11-26 03:57:33 +0300

    json

--- Scripts/degrees.js
+++ Scripts/degrees.js
@@ -1,11 +1,55 @@
 const data= "data/degrees.json"
 
 /* The function */
+//var mydegrees = [];
+// $(function() {
+
+
+//     // $.getJSON('https://github.com/typicode/json-server/blob/master/routes.json', function(data) {
+//     //    alert("AAA") ;
+//     //     mydegrees=JSON.parse(data.data);
+//     //     console.log(mydegrees);
+//     // });
+
+//     var txtFile = "D:/degrees.json";
+//     var file = new File([""],txtFile);
+    
+//     file.open("r"); // open file with read access
+//     var str = "";
+//     while (!file.eof) {
+//         // read each line of text
+//         str += file.readln() + "\n";
+//     }
+//     file.close();
+//     alert(str);
+ 
+//  });
 
 function CreateTableFromJSON() {
     let i;
-    const mydegrees = data;
-
+  
+    // var mydegrees = JSON.parse(data);;
+    // console.log("ddddddddddddddddddddddd"+data);
+    // var mydegrees = [
+    //     {
+    //         "Book ID": "1",
+    //         "Book Name": "Computer Architecture",
+    //         "Category": "Computers",
+    //         "Price": "125.60"
+    //     },
+    //     {
+    //         "Book ID": "2",
+    //         "Book Name": "Asp.Net 4 Blue Book",
+    //         "Category": "Programming",
+    //         "Price": "56.00"
+    //     },
+    //     {
+    //         "Book ID": "3",
+    //         "Book Name": "Popular Science",
+    //         "Category": "Science",
+    //         "Price": "210.40"
+    //     }
+    // ]
     // EXTRACT VALUE FOR HTML HEADER.
     // ('Book ID', 'Book Name', 'Category' and 'Price')
     var col = [];
---
+++ data/degrees.js
@@ -0,0 +1,32 @@
+
+  var mydegrees = [
+      {
+        "school": "Boston University",
+        "type": "MS",
+        "url": "https://www.bu.edu/",
+        "name": "Web Application Development(M.S.)",
+        "college": "CLASS",
+        "Year conferred": "(2020)",
+        "collegeName": "Metropolitan College"
+      },
+      {
+        "school": "Full Sail University",
+        "type": "BS",
+        "url": "https://www.fullsail.edu/",
+        "name": "Web Design & Development(B.S.)",
+        "college": "CLASS",
+        "Year conferred": "(2013)",
+        "collegeName": "Technology"
+      },
+      {
+        "school": "Johnson & Wales University",
+        "type": "AA",
+        "url": "https://www.jwu.edu/",
+        "name": "Culinary Arts (A.S.)",
+        "college": "CLASS",
+        "Year conferred": "(1993)",
+        "collegeName": "Culinary Arts"
+      }
+  
+    ];
+  
