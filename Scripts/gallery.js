1713a3a0844163accb41b7a3adec36bb44788b6b
jeff-mccommas <jeffmccommas@gmail.com>
2018-11-26 14:12:45 -0500

    fix validation errors

--- Scripts/gallery.js
+++ Scripts/gallery.js
@@ -1,19 +1,8 @@
-
-$(function() {
+$(function () {
     $("button.filter-btn").click((function () {
-        const grid = document.getElementById("grid");
+        const div = document.getElementById("div");
         const images = document.getElementsByTagName('img')
         $("img").fadeToggle("fast");
 
     }))
-    });
-
-
-// $(document).ready(function(){
-//     $("button").click(function(){
-//         $("#div1").fadeToggle();
-//         $("#div2").fadeToggle("slow");
-//         $("#div3").fadeToggle(3000);
-//     });
-// });
-$("p").hide();
+});
